f3d75e0e8c1d9d50af8c388aa151076ea758f26a
Pozok05 <a2308766@institutmontilivi.cat>
2024-03-06 13:40:19 +0100

    Classe Producte creada
    patata

---
+++ BotigaCistellaObj/Producte.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotigaCistellaObj
+{
+    internal class Producte
+    {
+        //ATRIBUTS
+        private string nom;
+        private double preu_sense_iva;
+        private int iva;
+        private int quantitat;
+
+        //CONSTRUCTORS
+        Producte()
+        {
+            nom = "";
+            preu_sense_iva = 0.0;
+            iva = 21;
+            quantitat = 0;
+        }
+        Producte(string nom, double preu_sense_iva)
+        {
+            this.nom = nom;
+            this.preu_sense_iva= preu_sense_iva;
+        }
+    }
+}
