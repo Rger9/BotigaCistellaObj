33bc44d3dcab1bc3e86651634d06abf5d66b024d
Roger Palmada <a2308765@institutmontilivi.cat>
2024-03-06 13:41:38 +0100

    Inicialització botiga

---
+++ BotigaCistellaObj/Botiga.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotigaCistellaObj
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Botiga
+    {
+        // ATRIBUTS
+        private string nomBotiga;
+        private Producte[] productes;
+        private int nElements;
+
+        // CONSTRUCTORS
+        public Botiga() 
+        {
+            productes = new Producte[10];
+        }
+
+
+    }
+}
