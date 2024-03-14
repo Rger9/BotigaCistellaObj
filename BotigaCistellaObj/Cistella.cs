84508b277a74977f0d433f7eec550e4b756906b3
Pozok05 <a2308766@institutmontilivi.cat>
2024-03-14 09:46:09 +0100

    Update Cistella.cs

--- BotigaCistellaObj/Cistella.cs
+++ BotigaCistellaObj/Cistella.cs
@@ -16,6 +16,9 @@ namespace BotigaCistellaObj
         private double diners;
         
         //CONTRUCTORS
+        /// <summary>
+        /// Constructor Per defecte
+        /// </summary>
         public Cistella()
         {
             this.botiga = null;
@@ -24,14 +27,21 @@ namespace BotigaCistellaObj
             this.nElements = 0;
             this.diners = 0;
         }
+        /// <summary>
+        /// Constructor Complet
+        /// </summary>
+        /// <param name="botiga">Objecte Botiga</param>
+        /// <param name="data">Objecte Datatime de la data</param>
+        /// <param name="productes">Taula de productes, comprova que cada producte existeixi a botiga abans de copiarlo</param>
+        /// <param name="diners">Double de diners, a cada producte inserit comprova que té suficients diners i els resta a aquest parametre</param>
         public Cistella(Botiga botiga, DateTime data, Producte[] productes, double diners)
         {
             this.botiga = botiga;
             this.data = data;
             this.productes = new Producte[productes.Length];
             bool no_diners = false;
-            int i;
-            for (i = 0; i < productes.Length && !no_diners; i++)
+            int i = 0;
+            while(i < productes.Length && !no_diners)
             {
                 if (botiga.BuscarProducte(productes[i]))
                 {
@@ -43,21 +53,31 @@ namespace BotigaCistellaObj
                     }
                     else no_diners = true;
                 }
+                i++;
             }
             this.nElements = i + 1;
             this.diners = diners;
         }
         //PROPIETATS
+        /// <summary>
+        /// Get i Set de Botiga
+        /// </summary>
         public Botiga Botiga
         {
             get { return botiga; }
             set { botiga = value; } 
         }
+        /// <summary>
+        /// Get i set de Data
+        /// </summary>
         public DateTime Data
         {
             get { return data; }
             set { data = value; }
         }
+        /// <summary>
+        /// Get i Set de la taula Productes, només fa el set si el producte existeix a la botiga i te suficients diners per comprar-lo
+        /// </summary>
         public Producte[] Productes
         {
             get { return this.productes; }
@@ -65,8 +85,8 @@ namespace BotigaCistellaObj
             {
                 this.productes = new Producte[value.Length];
                 bool no_diners = false;
-                int i;
-                for (i = 0; i < productes.Length && !no_diners; i++)
+                int i = 0;
+                while(i < productes.Length && !no_diners)
                 {
                     if (botiga.BuscarProducte(value[i]))
                     {
@@ -78,14 +98,21 @@ namespace BotigaCistellaObj
                         }
                         else no_diners = true;
                     }
+                    i++;
                 }
                 this.nElements = i + 1;
             }
         }
+        /// <summary>
+        /// Get de nElements
+        /// </summary>
         public int NElements
         {
             get { return this.nElements; }
         }
+        /// <summary>
+        /// Get i set de diners
+        /// </summary>
         public double Diners
         {
             get { return this.diners; }
@@ -93,6 +120,10 @@ namespace BotigaCistellaObj
         }
         
         //METODES PUBLICS
+        /// <summary>
+        /// Afegeix un producte a la taula de productes si es que existeix a botiga i tens suficients diners
+        /// </summary>
+        /// <param name="producte">Producte que volem comprar</param>
         public void ComprarProducte(Producte producte)
         {
             if(botiga.BuscarProducte(producte))
@@ -138,6 +169,27 @@ namespace BotigaCistellaObj
             }
 
         }
-        
+        public void Mostra()
+        {
+            Console.WriteLine(this.ToString());
+        }
+        public double CostTotal()
+        {
+            double diners = 0;
+            for(int i = 0; i < nElements; i++)
+            {
+                diners += productes[i].Preu() * productes[i].Quantitat;
+            }
+            return diners;
+        }
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < nElements; i++)
+            {
+                s += productes[i].ToString() + "\n";
+            }
+            return base.ToString();
+        }
     }
 }
